13f9b855a8af3507860d410fd8a1dfbd1e617916
Ritika Patil <patilritika1510@gmail.com>
2025-09-26 16:13:43 +0530

    Fix Vite config for Netlify

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
+const path = require('path');
+
+module.exports = defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist', // Netlify default
+  },
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+});
